dc1a9973eedc2dc313428a867e349db2f6e9a4df
abdulkadir.oruc <abdulkadir.oruc@bilyoner.com>
2025-10-15 14:37:55 +0300

    Initial Commit

---
+++ BilyonerCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BilyonerCase
+//
+//  Created by Abdulkadir Oruç on 15.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
