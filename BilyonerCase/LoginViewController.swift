0c04c99e6bc023dc1df4ca18fd66ec87fbca974a
abdulkadir.oruc <abdulkadir.oruc@bilyoner.com>
2025-10-16 10:15:33 +0300

    initialize app

---
+++ BilyonerCase/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  BilyonerCase
+//
+//  Created by Abdulkadir Oruç on 15.10.2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ BilyonerCase/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  BilyonerCase
+//
+//  Created by Abdulkadir Oruç on 15.10.2025.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
